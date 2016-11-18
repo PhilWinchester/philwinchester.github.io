b5c3cbdd8e21be441792edd891789dac274ffab3
Philip Winchester <philipwinc@gmail.com>
2016-11-17 22:17:42 -0500

    added flex to projects page

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-console.log("Scripts Loaded");
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function(){
-  console.log("Dom Loaded");
-
-  // randomBackgroundColor();
-
-  setInterval(function(){
-    // randomBackgroundColor();
-  }, 100000)
-
-  function randomBackgroundColor(){
-      document.body.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-  }
-
-});
+(() => {
+  console.log('');
+})();
