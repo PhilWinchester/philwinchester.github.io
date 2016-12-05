2f3d07488fc77cabbecef2bcbcb1ef6dae087899
Philip Winchester <philipwinc@gmail.com>
2016-12-04 22:50:01 -0500

    added css classes for code styling

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@
   //     .setPin(".about") // pins the element for the the scene's duration
   //     .addTo(controller);   // assign the scene to the controller
 
-  let pySpan = $("#python-txt")
+  let pySpan = $("#py-txt")
   let jsSpan = $("#js-txt")
 
   jsSpan.typed({
@@ -27,10 +27,20 @@
   });
   pySpan.typed({
     strings: [`
-      class Developer: <br>
-        def __init__ (self): <br>
-          self.hobbyist = True
-
+      <span><span class='pyDef'>class</span> <span class='pyFunc'>Developer: </span></span><br>
+      <span class='ind2'>  <span class='pyDef'>def</span> <span class='pyFunc'>__init__ (self): </span></span><br>
+      <span class='ind3'> self.hobbyist = <span class='pyChar'>True </span></span><br>
+      <span class='ind3'> self.enthusiast = <span class='pyChar'>True </span></span><br>
+      <span class='ind3'> self.num_projects = 4</span><br>
+      <span class='ind3'> self.avg_hours_of_sleep = 7</span><br>
+      <span class='ind2'><span class='pyDef'>def</span> <span class='pyFunc'>start_project (self, importance): </span> </span><br>
+      <span class='ind4'> num_cups = 0</span><br>
+      <span class='ind3'> <span class='pyDef'>for</span> c <span class='pyDef'>in</span> <span class='pyChar'>range</span>(importance): </span><br>
+      <span class='ind4'> num_cups++</span><br>
+      <span class='ind4'> friends--</span><br>
+      <span class='ind2'><span class='pyDef'>def</span> <span class='pyFunc'>finish_project (self): </span> </span><br>
+      <span class='ind3'> self.num_projects++</span><br>
+      <span class='ind3'> self.num_projects++</span><br>
     `],
     typeSpeed: 10,
   });
