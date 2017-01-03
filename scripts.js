05f259cd2bef8002d5efdc93e2bb7cfe7ddff9c9
Philip Winchester <philipwinc@gmail.com>
2017-01-03 14:00:21 -0500

    modified photos and started slideshow effect

--- scripts.js
+++ scripts.js
@@ -67,24 +67,25 @@
     });
   }
 
-  let imgArray = [
-    'http://placehold.it/300x200',
-    'http://placehold.it/200x100',
-    'http://placehold.it/400x300'],
-    curIndex = 0;
-    imgDuration = 3000;
+  let slideIndex = 0;
 
-  function slideShow() {
-      document.getElementById('slider').className += "fadeOut";
-      setTimeout(function() {
-          document.getElementById('slider').src = imgArray[curIndex];
-          document.getElementById('slider').className = "";
-      },1000);
-      curIndex++;
-      if (curIndex == imgArray.length) { curIndex = 0; }
-      setTimeout(slideShow, imgDuration);
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
-  slideShow();
+  showSlides();
 
 
 
